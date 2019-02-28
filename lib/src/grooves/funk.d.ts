a7f602da33b9e44f239c8624b7598bbd529a5cac
Felix Roos <roos@entrecode.de>
2019-02-28 12:51:42 +0100

    build

--- lib/src/grooves/funk.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const funk: {
-    name: string;
-    tempo: number;
-    chords: () => any;
-    bass: () => any;
-    kick: (t: any) => (number | number[])[][];
-    snare: (t: any) => (number | number[])[];
-    hihat: (t: any) => number[][];
-};
